9bf2c0de233fb819e8d425a80ea94539dcab5d59
prajapatigagan <gaganprajapati121@gmail.com>
2026-02-22 11:28:37 +0530

    second commit

--- src/main/java/com/example/learningRESTAPIs/controller/StudentController.java
+++ src/main/java/com/example/learningRESTAPIs/controller/StudentController.java
@@ -1,14 +1,28 @@
 package com.example.learningRESTAPIs.controller;
 
 import com.example.learningRESTAPIs.dto.StudentDto;
+import com.example.learningRESTAPIs.entity.Student;
+import com.example.learningRESTAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(4L,"Gagan","gagan1@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
+    }
+    @GetMapping("/student/{id}")
+    public StudentDto getStudentById(){
+        return new StudentDto(4l,"ramu","ramu@gmail.com");
     }
 }
--- src/main/java/com/example/learningRESTAPIs/repository/StudentRepository.java
+++ src/main/java/com/example/learningRESTAPIs/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.learningRESTAPIs.repository;
 
-public interface StudentRepository{
+import com.example.learningRESTAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
